ab9ec272caf819268b6ea85bd394ecf3093086a5
himanshusain0 <ht0257445@gmail.com>
2026-04-05 14:19:32 +0530

    add

---
+++ Olympic_project_web/app.py
@@ -0,0 +1,87 @@
+import streamlit as st 
+import pandas as pd
+import preprocessor 
+import helper
+
+df = pd.read_csv('athlete_events.csv')
+region_df = pd.read_csv('noc_regions.csv')
+df= preprocessor.preprocess(df, region_df)
+
+st.sidebar.title('Olympic Analysis')
+
+
+user_menu= st.sidebar.radio(
+    'Select an option',
+    ('Medal Tally' ,'Overall Analysis ', ' Country-wise Analysis' ,'Athlete wise  Analysis')
+)
+
+if user_menu =='Medal Tally':
+    st.sidebar.header("Medal Tally")
+
+    years,country = helper.country_year_list(df)
+    selected_year= st.sidebar.selectbox("Select Year" , years)
+    selected_country = st.sidebar.selectbox("Select Year" , country)
+
+    medal_tally = helper.fetch_medal_tally(df, selected_year, selected_country)
+    if selected_year =='Overall' and selected_year =='Overall':
+        st.title('Overall')
+    if selected_year != 'Overall' and selected_country == 'Overall':
+        st.title("Medal Tally in " + str(selected_year) + " Olympics")
+    if selected_year == 'Overall' and selected_country != 'Overall':
+        st.title(selected_country + " overall performance")
+    if selected_year != 'Overall' and selected_country != 'Overall':
+        st.title(selected_country + " performance in " + str(selected_year) + " Olympics")
+    st.table(medal_tally)
+
+
+
+if user_menu=="Overall Analysis ":
+    editions= df['Year'].unique().shape[0]-1
+    cities= df['City'].unique().shape[0]
+    sports= df['Sport'].unique().shape[0]
+    events= df['Event'].unique().shape[0]
+    athletes=df['Name'].unique().shape[0]
+    nations=df['region'].unique().shape[0]
+
+    st.title('Top Statistics')
+
+    col1,col2,col3 = st.beta_columns(3)
+
+    with col1:
+        st.header("Editions")
+        st.title(editions)
+    with col2:
+        st.header("Hosts")
+        st.title(cities)
+    with col3:
+        st.header("Sports")
+        st.title(sports)
+    
+
+    col1, col2,col3 = st.beta_columns(3)
+    with col1:
+        st.header("Events")
+        st.title(events)
+    with col2:
+        st.header("Nations")
+        st.title(nations)
+    with col3:
+        st.header("Athletes")
+        st.title(athletes)
+
+    nations_over_time = helper.data_over_time(df,'region')
+
+    fig = px.line(nations_over_time , x ="Editions" , y='region')
+    st.title("Participating Nations over the years")
+    st.plotly_chart(fig)
+
+
+    events_over_time = helper.data_over_time(df, 'Event')
+    fig = px.line(events_over_time, x ="Edition", y ="Event")
+    st.title("Events over the years")
+    st.plotly_chart(fig)
+
+    
+
+
+
